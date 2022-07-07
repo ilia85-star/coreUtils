e70f5886fa306c2389384a93dcf9ec2d6757e57d
Ilia Mahjour-Shafiei <ilia.mahjour.shafiei@outlook.com>
2022-07-07 23:49:44 +0430

    add progressbar to mv and cp

--- addProg/addProg.sh
+++ addProg/addProg.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 case $1 in
-	mv) mv $2 $3 | pv $2 || printf '\e[A\e[K';;
-	cp) cp $2 $3 | pv $2;;
+	mv) mv "$2" "$3" | pv "$2" || printf '\e[A\e[K';;
+	cp) cp "$2" "$3" | pv "$2" ;;
 	*) echo "I didn't get it. :(";;
 esac
 
